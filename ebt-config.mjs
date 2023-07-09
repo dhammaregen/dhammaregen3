250a3dcf3425c099b20b4feba283a203cbf22e81
sabbamitta <43345786+sabbamitta@users.noreply.github.com>
2023-07-09 15:29:45 +0200

    Initial commit

---
+++ ebt-config.mjs
@@ -0,0 +1,14 @@
+const EbtConfig = {
+  "appName": "EBT-Site",
+  "basePath": "/",
+  "homePath": "#/wiki/welcome",
+  "content": {
+    "index": "toc",
+  },
+  "github": {
+    "account": "sc-voice",
+    "repository": "ebt-site3"
+  }
+}
+
+export default EbtConfig;
