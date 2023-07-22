734ecab0c1301ed6b58da29bd74635087487a3fe
sabbamitta <43345786+sabbamitta@users.noreply.github.com>
2023-07-22 21:17:08 +0200

    add monolingual

--- ebt-config.mjs
+++ ebt-config.mjs
@@ -2,6 +2,7 @@ const EbtConfig = {
   "appName": "Dhammaregen",
   "basePath": "/dhammaregen3/",
   "homePath": "#/wiki/startseite",
+  "monolingual": "de",
   "content": {
     "index": "inhalt",
   },
